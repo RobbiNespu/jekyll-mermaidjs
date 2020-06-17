412a9d685e2b5848aca5c3408d207876653b02db
Robbi Nespu <robbi.mohamad@ssi-schaefer.com>
2020-06-17 21:43:20 +0800

    Trying..

--- lib/jekyll/mermaidjs.rb
+++ lib/jekyll/mermaidjs.rb
@@ -4,5 +4,19 @@ module Jekyll
   module Mermaidjs
     class Error < StandardError; end
     # Your code goes here...
+    class MermaidChart < Liquid::Block
+            
+            def initialize(tag_name, markup, tokens)
+                super
+            end
+
+            def render(context)
+                @config = context.registers[:site].config['mermaid']
+                "<script src=\"#{@config['src']}\"></script>"\
+                "<script>mermaid.initialize({startOnLoad:true});</script>"\
+                "<div class=\"mermaid\">#{super}</div>"
+        end
   end
 end
+
+Liquid::Template.register_tag('mermaid', Jekyll::Mermaidjs:MermaidChart)
